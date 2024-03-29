c7c7d278330567cfc6712fc9ae7f2616548dbfcb
RebikHub <rebikovlk@mail.ru>
2024-03-30 00:52:54 +0200

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/pwa-geo-test/sw.js', { scope: '/pwa-geo-test/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
