0aed5beb0e969dd5d9cfaffe47c0b75c9aa9c8af
rebikovalexandr <alexandrreb22@gmail.com>
2024-03-29 18:39:42 +0200

    fix base

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import {VitePWA} from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
-export default defineConfig(({command}) => ({
+export default defineConfig(() => ({
   plugins: [react(), VitePWA({
     registerType: 'autoUpdate',
     strategies: 'injectManifest',
@@ -31,5 +31,5 @@ export default defineConfig(({command}) => ({
       ]
     }
   })],
-  base: command !== 'serve' ? './pwa-geo-test' : './pwa-geo-test',
+  // base: command !== 'serve' ? './pwa-geo-test' : './pwa-geo-test',
 }))
