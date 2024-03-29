a862a99ad13ef8c3da52b550e9b60058ec62fee5
rebikovalexandr <alexandrreb22@gmail.com>
2024-03-29 18:04:56 +0200

    update sw

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,13 @@
+import { clientsClaim, skipWaiting } from 'workbox-core';
+import { cleanupOutdatedCaches, precacheAndRoute } from 'workbox-precaching';
+
+cleanupOutdatedCaches();
+
+precacheAndRoute(self.__WB_MANIFEST || []);
+
+skipWaiting();
+clientsClaim();
+
 self.addEventListener('message', (event) => {
     console.log('message', event.data.coords);
 })
