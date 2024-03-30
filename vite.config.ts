ddbca78fa4e56b3ca4a70de2c2f7d0ccedba8e6e
RebikHub <rebikovlk@mail.ru>
2024-03-30 02:03:34 +0200

    base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
-export default defineConfig(() => ({
+export default defineConfig(({ command }) => ({
+  base: command !== 'serve' ? '/pwa-geo-test/' : '/',
   plugins: [react(), VitePWA({
     registerType: 'autoUpdate',
     strategies: 'injectManifest',
