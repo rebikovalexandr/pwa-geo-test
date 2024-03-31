085185563aee500ce1b8b5951d3e72e3072e623f
RebikHub <rebikovlk@mail.ru>
2024-03-31 16:29:05 +0200

    add watch position & update style

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,43 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import { OlMap } from './OlMap';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { OlMap } from "./OlMap";
 
 function App() {
   const [coords, setCoords] = useState({
-    latitude: 0, longitude: 0
-  })
+    latitude: 0,
+    longitude: 0,
+  });
 
   useEffect(() => {
-    if ('geolocation' in navigator) {
-      navigator.geolocation.getCurrentPosition(function(position) {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.watchPosition((position) => {
         // Получение координат пользователя
         const latitude = position.coords.latitude;
         const longitude = position.coords.longitude;
 
         setCoords({
-          latitude, longitude
-        })
-    
+          latitude,
+          longitude,
+        });
+
         // Отправка координат в сервис-воркер
         if (navigator.serviceWorker.controller) {
-          navigator.serviceWorker.controller.postMessage({ 
-            type: 'geolocation',
-            coords: { latitude, longitude }
+          navigator.serviceWorker.controller.postMessage({
+            type: "geolocation",
+            coords: { latitude, longitude },
           });
         }
       });
+    } else {
+      alert("Ваше устройство не поддерживает API геолокации.");
     }
-  }, [])
+  }, []);
 
+  useEffect(() => {
+    Notification.permission;
+  }, []);
 
-  return (
-
-    <OlMap latitude={coords.latitude} longitude={coords.longitude}/> 
-
-
-  )
+  return <OlMap latitude={coords.latitude} longitude={coords.longitude} />;
 }
 
-export default App
+export default App;
